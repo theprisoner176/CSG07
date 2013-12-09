e02ff1aa36c60e8fe29cd9e843b698d5bb8f700f
Mark Pitman <Mark_Pitman@marks-imac.home>
2013-12-09 23:58:21 +0000

    mapspage.php

---
+++ SRC/Drafts/Website/HTML/mapspage.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<script src="js/prototype.js" type="text/javascript"></script>
+<script src="js/scriptaculous.js?load=effects,builder" type="text/javascript"></script>
+<script src="js/lightbox.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="walktour.css"/>
+<link href='http://fonts.googleapis.com/css?family=Jacques+Francois' rel='stylesheet' type='text/css' />
+</head>
+<body>
+<div id="title">
+<h1>Walktour - Walks</h1>
+</div>
+<div id="login">
+<p><a href="home.html"><img src="home.png" alt="Home"></a></p>
+<p><a href="mapspage.html"><img src="maps.png" alt="Walks"></a></p>
+</div>
+<div id="main">
+<p class="center">Aberystwyth Pub Crawl</p>
+<p class="center">
+<iframe width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Aberystwyth&amp;aq=0&amp;oq=aber&amp;sll=52.414468,-4.077641&amp;sspn=0.028165,0.084028&amp;ie=UTF8&amp;hq=&amp;hnear=Aberystwyth,+Ceredigion,+United+Kingdom&amp;ll=52.414461,-4.077644&amp;spn=0.028165,0.084028&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+</p>
+<p class="headings">
+Description
+</p>
+<p class="left">
+A cruise through the town pub to pub. This will show you the town as you walk round and experience fine ales and a sea breeze.
+</p>
+<p class="left">
+<a href="images/example_1.jpg"rel="lightbox"><img src="images/example_1_thumb.jpg" width="100" height="40" alt="" /></a>
+</p>
+</div>
+</body>
+</html>
