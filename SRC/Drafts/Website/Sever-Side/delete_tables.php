16becdae8d44d5384d1663fa0bfddd793364b1ca
Sope David Adejumo <mda@aber.ac.uk>
2013-12-11 22:38:50 +0000

    To delete tables and commands needed to view the database

--- SRC/Drafts/Website/Sever-Side/delete_tables.php
+++ SRC/Drafts/Website/Sever-Side/delete_tables.php
@@ -1,16 +1,12 @@
 <?php
-$con=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14");
+$con=mysql_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14");
 if(! $con ){
   die('Could not connect: ' . mysql_error());
 }
 
 echo 'Connected successfully<br />';
-$sql = "DROP TABLE Location";
-mysql_select_db( 'csgp07_13_14' );
-$retval = mysql_query( $sql, $con );
-if(! $retval ){
-  die('Could not delete table: ' . mysql_error());
-}
+$sql = "DROP TABLE IF EXISTS csgp07_13_14 . Location";
+mysql_query($sql, $con) or die(mysql_error());
 echo "Table deleted successfully\n";
 mysql_close($con);
 ?>
