347ff6ef77764d61344d1f9d53a51809a5665584
Ryan-Gouldsmith <ryg1@aber.ac.uk>
2014-01-01 23:08:32 +0000

    Made it now so that we can see tables correctly from the database, for testing purposes. See issue #11 for TODO improvements

--- SRC/Drafts/Website/Sever-Side/list_table.php
+++ SRC/Drafts/Website/Sever-Side/list_table.php
@@ -1,25 +1,64 @@
 <?php
-session_save_path('../web/sessions');
-session_start();
+	session_save_path('../web/sessions');
+	session_start();
 	if(!isSet($_SESSION['gpadmin'])){
 		header("Location: logon.php");	
 	}
 	$con=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14");
-
-	  $res = mysqli_query($con,"SHOW TABLES");
+	$LIST_OF_WALKS = "List_of_Walks";
+	$LOCATION = "Location";
+	$PHOTO = "Photo";
+	$DESCRIPTION = "Description";
+	send_table_query($LIST_OF_WALKS, $con);
+	echo "<br>";
+	send_table_query($LOCATION, $con);
+	echo "<br>";
+	send_table_query($PHOTO, $con);
+	echo "<br>";
+	send_table_query($DESCRIPTION, $con);
+	
+	
+	function send_table_query($foo, $con){
+		show_table(mysqli_query($con, "SELECT * FROM $foo"), $foo);
+	}
+	
+	
+	function show_table($show_value, $foo){
+		if($show_value == NULL){
+				echo("error showing this database");
+		} else{
+			echo "<h2>$foo</h2>";
+			echo "<table border='1'>";
+			while ($foo = mysqli_fetch_array($show_value, MYSQLI_ASSOC)){
+				echo "<tr>";
+				foreach($foo as $key => $value){
+					echo "<td>" . $value . "</td>";
+				}
+			}
+			echo "</tr>";
+			echo "</table>";
+		}
+	}
+		
+	
+	
+	  /*$res = mysqli_query($con,"SHOW TABLES");
 	  $field;
 	  $fieldNo = 0;
 	  while($cRow = mysqli_fetch_array($res)){
 	    echo "<p>" . $cRow[0] . "</p>";
+	    
 		$field = mysqli_query($con,"SELECT * FROM $cRow[0]");
 
 		echo "<table border='1'>";//list every field in the table
 		while($row = mysqli_fetch_array($field)){
-			echo "<td>" . $row[$fieldNo] . "</td>";
-				$fieldNo++;
+			echo "<td>" . $row["id"] . "</td>";
+				
 		}
+		
 		echo "</tr>";
 		}
+		* */
 	
 	mysqli_close($con);
 			echo <<<EOT
@@ -32,7 +71,8 @@ session_start();
 			<a href = "field_form.php">Add Data to Fields</a><br>
 			<a href = "admin_login.php">Create Tables</a><br>
 			<a href = "logon.php">Logout Page</a>
-EOT;
+			EOT;
+
 	  
 
 ?>
