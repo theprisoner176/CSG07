8bef572c8c7380b8da82472ede7c3c0cf811e1cf
Ryan-Gouldsmith <ryg1@aber.ac.uk>
2014-01-27 13:47:26 +0000

    Index Page for Diagram 4.1 in the Project Management, no style.

---
+++ SRC/Drafts/Website/HTML/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Aber Tours</title>
+</head>
+<body>
+	<header>
+		<section>
+			<h1> Aber Tours</h1>
+		</section>
+	</header>
+	<section>
+	
+		<nav>
+			<ul>
+				<li> Home </li>
+				<li> View List of Walks </li>
+			</ul>
+		</nav>
+	</section>
+	<section>
+		<h4> About Us</h4>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam commodo mi feugiat tellus laoreet, et mattis justo condimentum. Duis risus mauris, luctus sed condimentum quis, vehicula eu est. Vivamus imperdiet lobortis diam et tristique. Pellentesque consequat dui nec massa ultricies, vitae convallis felis faucibus. Nam quis venenatis nunc. Nunc commodo libero tristique justo pretium luctus non sit amet elit. Fusce pretium vitae nibh ac ultrices. Maecenas iaculis tincidunt leo, vel bibendum odio interdum nec. Ut placerat egestas tortor, id fringilla felis laoreet nec. Curabitur nunc sapien, commodo at nibh eu, fermentum dapibus turpis. </p>
+
+		<p>Donec pellentesque placerat turpis nec faucibus. Maecenas pellentesque velit id vehicula vestibulum. Aenean sit amet dui ac neque varius pellentesque. Vestibulum pharetra suscipit egestas. Quisque nec ligula turpis. Curabitur malesuada mollis auctor. Nam fermentum odio molestie purus sodales sagittis. Donec at volutpat lacus. Maecenas suscipit quam sit amet hendrerit pharetra. Phasellus hendrerit eu neque posuere faucibus. Quisque quis sapien tellus. Nullam et placerat ante. Nullam a nisi non sapien viverra vestibulum at nec felis. Curabitur hendrerit ultricies ante, a mollis metus pretium in. Sed pharetra interdum nisi, non malesuada purus rutrum vel. Fusce nulla leo, consectetur nec egestas vel, blandit at dolor.</p>
+	</section>
+</body>
+</html>
