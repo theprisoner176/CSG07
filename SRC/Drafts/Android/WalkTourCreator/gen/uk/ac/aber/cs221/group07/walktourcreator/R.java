1def3fccb6031477ab898645be985a12e68af290
theprisoner176 <HarryBuckley@hh1hfb1.stunet.aber.ac.uk>
2014-01-27 14:26:16 +0000

    gen R stuff

--- SRC/Drafts/Android/WalkTourCreator/gen/uk/ac/aber/cs221/group07/walktourcreator/R.java
+++ SRC/Drafts/Android/WalkTourCreator/gen/uk/ac/aber/cs221/group07/walktourcreator/R.java
@@ -215,51 +215,45 @@ containing a value of this type.
         public static final int ic_plusone_tall_off_client=0x7f02001c;
     }
     public static final class id {
-        public static final int action_settings=0x7f05001b;
-        public static final int add_point_of_interest=0x7f05000d;
-        public static final int btn_close_popup=0x7f05001a;
-        public static final int button1=0x7f050007;
-        public static final int button2=0x7f05000f;
-        public static final int button3=0x7f05000e;
-        public static final int gallery1=0x7f050008;
+        public static final int action_settings=0x7f050018;
+        public static final int add_point_of_interest=0x7f05000a;
+        public static final int btn_close_popup=0x7f050017;
+        public static final int button1=0x7f050008;
+        public static final int button2=0x7f05000c;
+        public static final int button3=0x7f05000b;
         public static final int hybrid=0x7f050004;
-        public static final int localWalks=0x7f050011;
-        public static final int long_description_input=0x7f050015;
-        public static final int map=0x7f05000c;
-        public static final int map_view=0x7f050010;
+        public static final int localWalks=0x7f05000e;
+        public static final int long_description_input=0x7f050012;
+        public static final int map=0x7f050009;
+        public static final int map_view=0x7f05000d;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
-        public static final int open_options_screen=0x7f05000b;
-        public static final int poi_description=0x7f050006;
-        public static final int poi_title=0x7f050005;
-        public static final int popup_element=0x7f050018;
+        public static final int open_options_screen=0x7f050007;
+        public static final int popup_element=0x7f050015;
         public static final int satellite=0x7f050002;
-        public static final int serverSideWalks=0x7f050012;
-        public static final int short_description_input=0x7f050016;
-        public static final int start_new_walk=0x7f050009;
-        public static final int submit=0x7f050014;
+        public static final int serverSideWalks=0x7f05000f;
+        public static final int short_description_input=0x7f050013;
+        public static final int start_new_walk=0x7f050005;
+        public static final int submit=0x7f050011;
         public static final int terrain=0x7f050003;
-        public static final int textView1=0x7f050013;
-        public static final int title_input=0x7f050017;
-        public static final int txtView=0x7f050019;
-        public static final int view_previous_walks=0x7f05000a;
+        public static final int textView1=0x7f050010;
+        public static final int title_input=0x7f050014;
+        public static final int txtView=0x7f050016;
+        public static final int view_previous_walks=0x7f050006;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_add_poi_dialog=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
-        public static final int activity_map_screen=0x7f030002;
-        public static final int activity_my_walks_screen=0x7f030003;
-        public static final int activity_options_screen=0x7f030004;
-        public static final int activity_walk_setup_screen=0x7f030005;
-        public static final int popup_finish_walk=0x7f030006;
+        public static final int activity_main_menu=0x7f030000;
+        public static final int activity_map_screen=0x7f030001;
+        public static final int activity_my_walks_screen=0x7f030002;
+        public static final int activity_options_screen=0x7f030003;
+        public static final int activity_walk_setup_screen=0x7f030004;
+        public static final int popup_finish_walk=0x7f030005;
     }
     public static final class menu {
-        public static final int add_poi_dialog=0x7f0a0000;
-        public static final int add_poi_screen=0x7f0a0001;
-        public static final int login_screen=0x7f0a0002;
+        public static final int login_screen=0x7f0a0000;
     }
     public static final class string {
         public static final int action_settings=0x7f06001b;
