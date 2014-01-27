432a4bbd054086658ec1240aa0783ad81644b263
jackreeve <jar39@aber.ac.uk>
2014-01-27 12:49:20 +0000

    Added points to the map.

--- SRC/Drafts/Website/HTML/walk_details.php
+++ SRC/Drafts/Website/HTML/walk_details.php
@@ -21,11 +21,27 @@ function initialize() {
     zoom: 8,
     center: new google.maps.LatLng(52.416667, -4.066667)
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-}
 
+
+  var myLatLng = new google.maps.LatLng(52.416667, -4.066667);
+  var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Aberystwyth'
+  });
+
+  var myLatLng = new google.maps.LatLng(52.7077, -2.7541);
+  var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Shrewsbury'
+  });
+
+}
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
     </script>
 </head>
