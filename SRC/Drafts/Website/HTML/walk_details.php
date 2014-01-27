d571f05b85c26ae0a202d6248f6a65c6622bb23a
jackreeve <jar39@aber.ac.uk>
2014-01-27 13:28:51 +0000

    Added pop-up boxes to provide POI details.

--- SRC/Drafts/Website/HTML/walk_details.php
+++ SRC/Drafts/Website/HTML/walk_details.php
@@ -23,7 +23,58 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+      
+      
+      var contentString1 = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Shrewsbury</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+      
 
+var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Aberystwyth</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow1 = new google.maps.InfoWindow({
+      content: contentString1
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
 
   var myLatLng = new google.maps.LatLng(52.416667, -4.066667);
   var marker = new google.maps.Marker({
@@ -31,12 +82,20 @@ function initialize() {
       map: map,
       title: 'Aberystwyth'
   });
+  
+    google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
 
   var myLatLng = new google.maps.LatLng(52.7077, -2.7541);
-  var marker = new google.maps.Marker({
+  var marker1 = new google.maps.Marker({
       position: myLatLng,
       map: map,
       title: 'Shrewsbury'
+  });
+  
+    google.maps.event.addListener(marker1, 'click', function() {
+    infowindow1.open(map,marker1);
   });
 
   var flightPlanCoordinates = [
