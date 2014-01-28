204092ab7a6dcf3c77773fcf02cdf9f2adb78482
Ryan-Gouldsmith <ryg1@aber.ac.uk>
2014-01-28 11:10:48 +0000

    First blank functions for PHPUnit Testing

---
+++ SRC/Drafts/Testing/PHPUnit/DatabaseSet.php
@@ -0,0 +1,14 @@
+<?php
+	class DatabaseSet extends PHPUnit_Framework_TestCase { 
+		
+		public function check_query(){
+			
+		}
+		public function check_connection(){
+		
+		}
+		public function is_instance_check(){
+			
+		}
+	}
+?>
