1a5583def7bb0cbc8017b52ad0fdc2194169d0d3
Ryan-Gouldsmith <ryg1@aber.ac.uk>
2014-01-28 20:35:44 +0000

    Edited bits, bit more refined. Still not working properly. Hard coded data

--- SRC/Drafts/Website/HTML/walk_details.php
+++ SRC/Drafts/Website/HTML/walk_details.php
@@ -3,200 +3,22 @@
 <head>
 	<title>List of walks</title>
 	<link rel="stylesheet" type="text/css" href="../CSS/style.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="../CSS/print.css" media="print" /> 
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">  
 	  <style>
-      html, body, #map-canvas {
-        height: 100%;
+     html, body , #map-canvas {
+        height: 85%;
         margin: 0px;
         padding: 0px
       }
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMaDwxzucsIfiT1sYyFKWIvDljXOWSeM0&sensor=false"></script>
-    
     <link rel="stylesheet" type="text/css" href="shadowbox-3.0.3/shadowbox.css">
-<script type="text/javascript" src="shadowbox-3.0.3/shadowbox.js"></script>
-<script type="text/javascript">
-Shadowbox.init();
-</script>
-    
-    <script>
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(52.416667, -4.066667)
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-      
-      
-      var contentString1 = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Shrewsbury</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-      
-
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Aberystwyth</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow1 = new google.maps.InfoWindow({
-      content: contentString1
-  });
-
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  });
-
-  var myLatLng = new google.maps.LatLng(52.416667, -4.066667);
-  var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Aberystwyth'
-  });
-  
-    google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-
-  var myLatLng = new google.maps.LatLng(52.7077, -2.7541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.7077, -2.2541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.7077, -2.0541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.7077, -2.9541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.0077, -2.2541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.5077, -2.5541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.1077, -2.1541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.0077, -2.0541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    var myLatLng = new google.maps.LatLng(52.9077, -2.9541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-      var myLatLng = new google.maps.LatLng(52.7077, -2.4541);
-  var marker1 = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Shrewsbury'
-  });
-  
-    google.maps.event.addListener(marker1, 'click', function() {
-    infowindow1.open(map,marker1);
-  });
-
-  var flightPlanCoordinates = [
-    new google.maps.LatLng(52.416667, -4.066667),
-    new google.maps.LatLng(52.7077, -2.7541),
-    new google.maps.LatLng(52.7077, -2.2541),
-    new google.maps.LatLng(52.7077, -2.0541),
-    new google.maps.LatLng(52.7077, -2.9541),
-    new google.maps.LatLng(52.0077, -2.2541),
-    new google.maps.LatLng(52.5077, -2.5541),
-    new google.maps.LatLng(52.1077, -2.1541),
-    new google.maps.LatLng(52.0077, -2.0541),
-    new google.maps.LatLng(52.9077, -2.9541),
-    new google.maps.LatLng(52.7077, -2.4541),
-    new google.maps.LatLng(52.416667, -4.066667),   
-  ];
-  
-    var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-  });
-
-  flightPath.setMap(map);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-    </script>
+	<script type="text/javascript" src="shadowbox-3.0.3/shadowbox.js"></script>
+	<script type="text/javascript">
+		Shadowbox.init();
+	</script>
+   
 </head>
 <body>
 	<a href="Images/myimage.jpg" rel="shadowbox"><img src="Images/myimage.jpg" height="87" width="156" /></a>
@@ -205,20 +27,13 @@ google.maps.event.addDomListener(window, 'load', initialize);
 	</header>
 	<nav>
 		<ul>
-			<li class="selected"><a href="#">Home</a></li>
-			<li><a href="#">About</a></li>
-			<li></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">Support</a></li>
-			<li><a href="#">Log in</a></li>
-			<li></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="list_walks.php">List Walks</a></li>
 		</ul>
 	</nav>
 	<section id="intro"></section>
 		<div id="map-canvas"></div>	
-		
 						<?php
-						
 							include "database_layer.php";
 							///creates a new instance of the database walk option
 							$database = new DatabaseWalk();
@@ -241,11 +56,59 @@ google.maps.event.addDomListener(window, 'load', initialize);
 								echo "<br/>";
 								echo "This is the distance for the walk " . $walk['distance'];
 							}
-							$database->close_connection();							
-							
-							
+							$database->close_connection();										
 						?>
+						 <script>
+						var map;
+						function initialize() {
+						var mapOptions = {
+							zoom: 8,
+							center: new google.maps.LatLng(52.416667, -4.066667)
+						};
+						var map = new google.maps.Map(document.getElementById('map-canvas'),
+								mapOptions);
+						
+						var contentString = <?php?>'<div id="content">'+'<h1 id="firstHeading" class="firstHeading"></h1>'+
+						'<div id="bodyContent">'+'<p>short desc </p>'+'</div>'+'</div>';
+	
+						var infowindow = new google.maps.InfoWindow({
+							content: contentString
+						});
 
+						var myLatLng = new google.maps.LatLng(52.416667, -4.066667);
+						var marker = new google.maps.Marker({
+							position: myLatLng,
+							map: map,
+							title: 'Aberystwyth'
+						});
+  
+						google.maps.event.addListener(marker, 'click', function() {
+								infowindow.open(map,marker);
+						});
 
+						var myLatLng = new google.maps.LatLng(52.7077, -2.7541);
+						var marker = new google.maps.Marker({
+								position: myLatLng,
+								map: map,
+								title: 'Shrewsbury'
+						});
+  
+						var flightPlanCoordinates = [
+							new google.maps.LatLng(52.416667, -4.066667),
+							new google.maps.LatLng(52.7077, -2.7541),   
+						];
+  
+						var flightPath = new google.maps.Polyline({
+							path: flightPlanCoordinates,
+							geodesic: true,
+							strokeColor: '#FF0000',
+							strokeOpacity: 1.0,
+							strokeWeight: 2
+						});
+
+						flightPath.setMap(map);
+						}
+						google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 </body>
 </html>
