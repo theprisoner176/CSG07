62ab679c2b41594378b07180edca45997234d68d
Sope David Adejumo <mda@aber.ac.uk>
2014-01-28 15:22:01 +0000

    Added data to add poi's. By mrp2

--- SRC/Drafts/Website/Sever-Side/add_fields.php
+++ SRC/Drafts/Website/Sever-Side/add_fields.php
@@ -2,53 +2,61 @@
 session_save_path('../web/sessions');
 session_start();
 if(!isSet($_SESSION['gpadmin'])){
-		header("Location: logon.php");	
-	}
+                header("Location: logon.php");        
+        }
 
-	$walk_id; 
-	$walk_title; 
-	$walk_short; 
-	$walk_long; 
-	$walk_hours;
-	$walk_distance;
-	/**
-	 * checks to see if it's set, no point in doing it if it's not
-	 */
-	 $con=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14") or die("Couldn't connect");
-	if (isset($_POST["id"])){
-			$walk_id = mysqli_real_escape_string($con,$_POST['id']);
-	}
-	if (isset($_POST["title"])){
-			$walk_title = mysqli_real_escape_string($con,$_POST['title']);
-	}
-	if (isset($_POST["sh_de"])){
-			$walk_short = mysqli_real_escape_string($con,$_POST['sh_de']);
-	}
-	if (isset($_POST["l_de"])){
-			$walk_long = mysqli_real_escape_string($con,$_POST['l_de']);
-	}
-	if (isset($_POST["hours"])){
-			$walk_hours = mysqli_real_escape_string($con,$_POST['hours']);
-	}
-	if (isset($_POST["distance"])){
-			$walk_distance = mysqli_real_escape_string($con,$_POST['distance']);
-	}
+        $walk_id; 
+        $walk_title; 
+        $walk_short; 
+        $walk_long; 
+        $walk_hours;
+        $walk_distance;
+        $walk_Latt;
+        $walk_Longitude;
+        /**
+         * checks to see if it's set, no point in doing it if it's not
+         */
+         $con=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14") or die("Couldn't connect");
+        if (isset($_POST["id"])){
+                        $walk_id = mysqli_real_escape_string($con,$_POST['id']);
+        }
+        if (isset($_POST["title"])){
+                        $walk_title = mysqli_real_escape_string($con,$_POST['title']);
+        }
+        if (isset($_POST["sh_de"])){
+                        $walk_short = mysqli_real_escape_string($con,$_POST['sh_de']);
+        }
+        if (isset($_POST["l_de"])){
+                        $walk_long = mysqli_real_escape_string($con,$_POST['l_de']);
+        }
+        if (isset($_POST["hours"])){
+                        $walk_hours = mysqli_real_escape_string($con,$_POST['hours']);
+        }
+        if (isset($_POST["distance"])){
+                        $walk_distance = mysqli_real_escape_string($con,$_POST['distance']);
+        }
+        if (isset($_POST["Latt"])){
+                        $walk_Latt = mysqli_real_escape_string($con,$_POST['latt']);     
+        }
+        if (isset($_POST["Longitude"])){
+                        $walk_Longitude = mysqli_real_escape_string($con,$_POST['Long']);
+        }
+       
+        
+        /**
+         * Adds the information as new records to the Walks Table
+         */
+        $query = "INSERT INTO List_of_Walks (id, title, shortDesc, longDesc, hours, distance) VALUES 
+        ('$walk_id','$walk_title','$walk_short', '$walk_long', '$walk_hours', '$walk_distance')";
 
-	
-	/**
-	 * Adds the information as new records to the Walks Table
-	 */
-	$query = "INSERT INTO List_of_Walks (id, title, shortDesc, longDesc, hours, distance) VALUES 
-	('$walk_id','$walk_title','$walk_short', '$walk_long', '$walk_hours', '$walk_distance')";
-
-	if (mysqli_query($con,$query)){
-		echo "Sent to the Database";
-	}
-	else{
-		echo "Could not add data" . mysqli_error($con);
-	}
-		echo'<p><a href = "list_table.php">View Tables</a><br>
-		<a href = "field_form.php">Add Data to Fields</a><br>
-		<a href = "logon.php">Logout Page</a></p>';
-		mysqli_close($con);	
+        if (mysqli_query($con,$query)){
+                echo "Sent to the Database";
+        }
+        else{
+                echo "Could not add data" . mysqli_error($con);
+        }
+                echo'<p><a href = "list_table.php">View Tables</a><br>
+                <a href = "field_form.php">Add Data to Fields</a><br>
+                <a href = "logon.php">Logout Page</a></p>';
+                mysqli_close($con);        
 ?>
