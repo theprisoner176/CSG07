fb621208e5063f6d7a84dd53057dee5c2a1d3a56
Sope David Adejumo <mda@aber.ac.uk>
2014-01-28 09:35:50 +0000

    JSON integration

--- SRC/Drafts/Website/Sever-Side/file_saver.php
+++ SRC/Drafts/Website/Sever-Side/file_saver.php
@@ -1,41 +1,51 @@
 <?php
-	$walk_title = $_POST["title"];//to do
-	
-	if(!isSet($_POST["walk_data"])){
-		echo "No Data Received";	
-	}
-	else{
-		$walk_conn=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14");
-	
-		if (mysqli_connect_errno()) {
-		  echo "Cannot Connect: " . mysqli_connect_error();
-		}
-		else{
-			echo "Connected";	
-		}
-			  
-		$json = $_REQUEST['json'];
-		echo "JSON: \n";
-		var_dump($json);
-		echo "\n\n";
+        
+       if(!isSet($_POST)){
+               echo "No Data Received";        
+        }
+        else{
+                $walk_conn=mysqli_connect("db.dcs.aber.ac.uk","csadmgp07","c54admgp07","csgp07_13_14") or die("Cannot Connect");
+        
 
-		$data = json_decode($json,true);
-		var_dump($data);
+                          
+                $file = fopen('data_file.txt','a+');	
+				$json = implode($_POST);
+				fwrite($file, $json);
+                $json_array = json_decode($json,true);
+                //fwrite($file, print_r($json_array['title']));
+        			foreach($json_array as $result => $val) {
+ 						fwrite($file, $result);
+ 						foreach($val as $arry){
+ 							fwrite($file, $arry->long_desc);
+ 						}
+              			//fwrite($file, $json_array[0]['walk'][0]['long_desc']);              			
+      			
+        			
+        		}
+        			
+        		//}
+				//else echo "\$json['results'] is not an array";
+                //fwrite($file, "JSON =" . $post_data . "\n");
+               // $shortDescription=$_POST['shortDesc'];
+               // $longDescription=$_POST['longDesc'];
+               // $photoName=$_POST['photoName'];
+               // $latitude=$_POST['latitude'];
+               // $longitude=$_POST['longitude'];
+                
 
-		
-		$shortDescription=$_POST['shortDesc'];
-		$longDescription=$_POST['longDesc'];
-		$photoName=$_POST['photoName'];
-		$coordinates=$_POST['coordinates'];
-		
 
-		//insert post data into tables 
-		$sql = "INSERT INTO createListOfWalksTable values($sortDescription,$longDescription,0)";
-		$sql = "INSERT INTO createPhotoTable values($photoName,0)";
-		
-		
-		mysql_query($sql);
+				fclose($file);
 
-	 mysqli_close($walk_conn);
-	}
+                //insert post data into tables 
+                $sql = "INSERT INTO List_of_Walks values($shortDescription,$longDescription,0)"; 
+                mysqli_query($walk_conn,$sql);            
+                $sql = "INSERT INTO Photo values($photoName,0)";
+                mysqli_query($walk_conn,$sql);
+                $sql = "INSERT INTO Location values($latitude,$longitude,0)";
+                mysqli_query($walk_conn,$sql);
+                
+                //mysql_query($sql);
+
+         mysqli_close($walk_conn);
+       }
 ?>
