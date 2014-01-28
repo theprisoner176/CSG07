c28698c1801aeb303d0328f68b85931e6a34b4c9
macdob <mwd5@aber.ac.uk>
2014-01-28 16:48:58 +0000

    junit test case

---
+++ SRC/Drafts/Android/WalkTourCreator/src/uk/ac/aber/cs221/group07/walktourcreator/tests/WalkModelTest.java
@@ -0,0 +1,62 @@
+package uk.ac.aber.cs221.group07.walktourcreator.tests;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class WalkModelTest extends TestCase {
+
+	public void testWalkModelString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetLastPoi() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testWalkModelIntStringVectorOfLocationPointStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetRoutePath() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetDistance() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetTimeTaken() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetShortDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetShortDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetLongDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetLongDescription() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testAddLocation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
