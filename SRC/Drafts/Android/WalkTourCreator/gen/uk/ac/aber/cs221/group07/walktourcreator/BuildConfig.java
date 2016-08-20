493b8411e9cf24b89db10b1f960fee5814fc5f26
Harry Flynn Buckley <buckley.harry@googlemail.com>
2016-08-20 21:58:15 +0100

    changed project hierarchy

--- SRC/Drafts/Android/WalkTourCreator/gen/uk/ac/aber/cs221/group07/walktourcreator/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package uk.ac.aber.cs221.group07.walktourcreator;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
